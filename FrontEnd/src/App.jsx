1c2012b5e53d61f15dbb5bcaf6b27197f27bc652
Thilagavathi Vijayan <106983063+Thilagavijayan@users.noreply.github.com>
2023-11-06 14:57:30 +0530

    Add files via upload

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,1 +1,26 @@
+// import Navbar from "./components/Navbar";
 
+import { useState, useEffect} from "react"
+
+
+const App = () => {
+
+  const [name, setName] = useState(0)
+
+  useEffect(() => {
+    console.log("use effect ran");
+  }),[name]
+  
+  const handlesubmit = () => {
+    setName(name + 1)
+  }
+  return ( 
+    <div className="app">
+      <h2>{name}</h2>
+      <button onClick={handlesubmit}>Like</button>
+      {/* <Navbar/> */}
+    </div>
+   );
+}
+ 
+export default App;
